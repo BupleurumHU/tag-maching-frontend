9671bc81fa80fd21006056c7bdb3c616ede94e4d
bupleurum <1105768334@qq.com>
2025-11-20 17:54:31 +0800

    初始化vite脚手架，添加Navbar导航栏以及Tabbar标签栏

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {showToast} from "vant";
+import {ref} from 'vue';
+
+const onClickLeft = () => alert('返回');
+const onClickRight = () => alert('右');
+
+const active = ref(0);
+const onChange = (index) => showToast(`标签 ${index}`);
+
+
+</script>
+
+<template>
+  <van-nav-bar title="标题" left-text="返回" left-arrow
+               @click-left="onClickLeft"
+               @click-right="onClickRight">
+    <template #right>
+      <van-icon name="search" size="18"/>
+    </template>
+  </van-nav-bar>
+
+  <van-tabbar v-model="active" @change="onChange">
+    <van-tabbar-item name="index" icon="home-o">主页</van-tabbar-item>
+    <van-tabbar-item name="team" icon="search">标签</van-tabbar-item>
+    <van-tabbar-item name="user" icon="friends-o">个人</van-tabbar-item>
+  </van-tabbar>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import {createApp} from 'vue'
+// import './style.css'
+import App from './App.vue'
+import {Button, NavBar, Tabbar, TabbarItem} from 'vant';
+
+const app = createApp(App);
+app.use(Button);
+app.use(NavBar);
+app.use(Tabbar);
+app.use(TabbarItem);
+
+app.mount('#app')
+
+
+
+
+
+
+
+
